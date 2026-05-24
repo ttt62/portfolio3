d1a0bef5ddec85bb8c7b818d5657cec8f99a652d
ttt62 <p2320132@myu.ac.jp>
2026-05-24 23:51:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+btn1 = $('#button1');
+target = $('#target_div');
+
+btn1.click(
+  function () {
+    target.toggle();
+  }
+);
+btn2 = $('#button2');
+target2=$('#target_div2')
+btn2.click(
+  function () {
+    target2.toggle();
+  }
+);
+btn3 = $('#button3');
+target3=$('#target_div3')
+btn3.click(
+  function () {
+    target3.toggle();
+  }
+);
+btn4 = $('#button4');
+target4=$('#target_div4')
+btn4.click(
+  function () {
+    target4.toggle();
+  }
+);
+btn5 = $('#button5');
+target5=$('#target_div5')
+btn5.click(
+  function () {
+    target5.toggle();
+  }
+);
